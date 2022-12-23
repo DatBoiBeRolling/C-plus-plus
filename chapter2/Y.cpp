0e0d0dbe2897f212f705cc1486b8fb27a6f462c2
Hunter Li <119752957+DatBoiBeRolling@users.noreply.github.com>
2022-12-23 13:19:24 -0500

    Add files via upload

---
+++ chapter2/Y.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// Binary
+	cout << 0b01011001 << endl << char (0b01011001);
+	
+
+	// Octal
+	cout << 0131 << " ";
+	cout << char(0131) << endl;
+
+	// Decimal
+	cout << 89 << " ";
+	cout << char(89) << endl;
+
+	// Hexadecimal
+	cout << 0X59 << " ";
+	cout << char(0X59) << endl;
+}
