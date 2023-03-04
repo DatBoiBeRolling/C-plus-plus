bef373cc4be8528ec2c4e82c46f60317d7ecab8c
Hunter Li <lihunter2008@gmail.com>
2023-03-03 21:41:58 -0500

    chapter4

--- chapter4/mytitle.cpp
+++ chapter4/mytitle.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-
 using namespace std;
 
 int main() {
     string input;
-    cout << "Enter a string: ";
+    cout << "Enter a string: "; // Input
     getline(cin, input);
     
 	bool newWord = true; // Flag
 
-    for (char& c: input) {
-        if (newWord && isalpha(c)) {
-            c = toupper(c);
+    for (char& x: input) {
+        if (newWord && isalpha(x)) {
+            x = toupper(x);
             newWord = false;
         }
-        else if (isspace(c) || ispunct(c) || isdigit(c)) {
+        else if (isspace(x) || ispunct(x) || isdigit(x)) {
             newWord = true;
         }
     }
