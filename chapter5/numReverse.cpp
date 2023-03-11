4cf45bed956181574bd0c03869279bcc7d47c337
Hunter Li <lihunter2008@gmail.com>
2023-03-11 17:23:21 -0500

    Loop stuff

--- chapter5/numReverse.cpp
+++ chapter5/numReverse.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 	int input1, inpL;
-	string input2, output;
+	string input2;
 	cout << "Enter a positive integer: ";
 	cin >> input1;
 
-	while (input1<0)
+	while (input1 < 0)
 	{
 		cout << "That is not a positive integer, please try again: ";
 		cin >> input1;
@@ -18,12 +18,11 @@ int main()
 	input2 = to_string(input1);
 	inpL = input2.length();
 
-	while (inpL>=0)
+	while (inpL >= 0)
 	{
-		output += input2[inpL];
+		cout << input2[inpL] ;
 		inpL--;
 	}
-
-	cout << output << endl;
+	cout << endl;
 	return 0;
 }
