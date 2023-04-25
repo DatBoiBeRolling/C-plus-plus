d1a5f185f5be8ebc68a332ebf81f94bde2320fe7
Hunter Li <lihunter2008@gmail.com>
2023-04-25 19:49:40 -0400

    FIRST ALGORITHM

---
+++ chapter8/BubbleSortAlgorithm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double data[] = {128, 2, 16, -33, 1.5, 0, -8, -156};     // Enter data here
+
+  int size = sizeof(data) / sizeof(data[0]);
+  
+   for (int cycle = 0; cycle < size -1; cycle++) {     // Locks numbers in
+    for (int element = 0; element < size - cycle - 1; element++) { // Goes through entire array
+      if (data[element] > data[element + 1]) {  // Compare
+        swap(data[element],data[element+1]);    // Swap
+      }
+    }
+  }
+  
+  cout << "Here is the array in ascending order:\n";  
+  
+    for (int i = 0; i < size; i++) {
+    cout << data[i] << " ";
+  }
+
+  cout << endl;
+}
